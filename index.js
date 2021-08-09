62b6704a39350e9625b7a8ba80a4b70dcd932af0
siarheichura <chura14@yandex.ru>
2021-08-09 22:25:48 +0300

    feat: add weather in users geolocation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// DOM elements
 const citySelect = document.querySelector('.weather__city-select')
 const city = document.querySelector('.weather__city')
 const temperature = document.querySelector('.weather__temperature')
@@ -7,29 +6,49 @@ const feelsLike = document.querySelector('.weather__feelslike')
 const windSpeed = document.querySelector('.weather__wind-speed')
 const icon = document.querySelector('.weather__icon')
 
-// Array of cities weather of which we can show 
 let cities = ['Minsk', 'Brest', 'Grodno', 'Vitebsk', 'Mogilev', 'Gomel']
 
-setCitiesInDrop()
-showMeWeather(citySelect.value)
+document.addEventListener("DOMContentLoaded", app)
 
-citySelect.addEventListener('change', event => {
-    showMeWeather(event.target.value)
-})
-
-async function showMeWeather(cityName) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b5a03b378e49085452cb14ec5350c1e9`)
-    let weatherDB = await response.json()
-
-    city.innerText = weatherDB.name
-    temperature.innerText = convertCalvinToCelsius(weatherDB.main.temp)
-    weatherStatus.innerText = weatherDB.weather[0].description
-    feelsLike.innerText = `Feels like: ${convertCalvinToCelsius(weatherDB.main.feels_like)}`
-    windSpeed.innerText = `Wind speed: ${weatherDB.wind.speed}m/s`
-    icon.src = `https://openweathermap.org/img/wn/${weatherDB.weather[0].icon}@2x.png`
+function app() {
+    setCitiesInDrop()
+    showWeatherInUserGeo()
+    initDropdownListener()
 }
 
-// Push cities from array 'cities' to dropdown
+function initDropdownListener() {
+    citySelect.addEventListener('change', event => {
+        showWeatherInChosenCity(event.target.value)
+    })
+}
+
+function showWeatherInUserGeo() {
+    navigator.geolocation.getCurrentPosition(position => {
+        let latitude = position.coords.latitude.toFixed(4)
+        let longitude = position.coords.longitude.toFixed(4)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b5a03b378e49085452cb14ec5350c1e9`)
+        .then(response => response.json())
+        .then(result => {
+            renderWeather(result)
+        })
+    })
+}
+
+async function showWeatherInChosenCity(cityName) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b5a03b378e49085452cb14ec5350c1e9`)
+    let weatherDB = await response.json()
+    renderWeather(weatherDB)
+}
+
+function renderWeather(DB) {
+    city.innerText = DB.name
+    temperature.innerText = convertCalvinToCelsius(DB.main.temp)
+    weatherStatus.innerText = DB.weather[0].description
+    feelsLike.innerText = `Feels like: ${convertCalvinToCelsius(DB.main.feels_like)}`
+    windSpeed.innerText = `Wind speed: ${DB.wind.speed}m/s`
+    icon.src = `https://openweathermap.org/img/wn/${DB.weather[0].icon}@2x.png`
+}
+
 function setCitiesInDrop() {
     cities.forEach(city => {
         let option = new Option(city, city)
@@ -37,7 +56,6 @@ function setCitiesInDrop() {
     })
 }
 
-// The temperature is stored in kelvin, so we need to convert it to celsius
 function convertCalvinToCelsius(temp) {
     return `${Math.round(temp - 273.15)}℃`
 }
