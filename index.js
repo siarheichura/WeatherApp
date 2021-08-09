66f6bcb51c08c46b722560857032810015118268
siarheichura <chura14@yandex.ru>
2021-08-09 20:49:28 +0300

    change API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const citySelect = document.querySelector('.weather__city-select')
 const city = document.querySelector('.weather__city')
 const temperature = document.querySelector('.weather__temperature')
+const weatherStatus = document.querySelector('.weather__status')
 const feelsLike = document.querySelector('.weather__feelslike')
 const windSpeed = document.querySelector('.weather__wind-speed')
 const icon = document.querySelector('.weather__icon')
@@ -16,6 +17,19 @@ citySelect.addEventListener('change', event => {
     showMeWeather(event.target.value)
 })
 
+async function showMeWeather(cityName) {
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b5a03b378e49085452cb14ec5350c1e9`)
+    let weatherDB = await response.json()
+
+    city.innerText = weatherDB.name
+    temperature.innerText = convertCalvinToCelsius(weatherDB.main.temp)
+    weatherStatus.innerText = weatherDB.weather[0].description
+    feelsLike.innerText = `Feels like: ${convertCalvinToCelsius(weatherDB.main.feels_like)}`
+    windSpeed.innerText = `Wind speed: ${weatherDB.wind.speed}m/s`
+    icon.src = `http://openweathermap.org/img/wn/${weatherDB.weather[0].icon}@2x.png`
+}
+
+// Push cities from array 'cities' to dropdown
 function setCitiesInDrop() {
     cities.forEach(city => {
         let option = new Option(city, city)
@@ -23,14 +37,7 @@ function setCitiesInDrop() {
     })
 }
 
-function showMeWeather(cityName) {
-    fetch(`http://api.weatherstack.com/current?access_key=c81a958a4d7a497211fc297e872058f0&query=${cityName}`)
-    .then(response => response.json())
-    .then(result => {
-        city.innerText = result.location.name
-        temperature.innerText = `${result.current.temperature}℃`
-        feelsLike.innerText = `Feels like: ${result.current.feelslike}℃`
-        windSpeed.innerText = `Wind speed: ${result.current.wind_speed}km/h`
-        icon.src = result.current.weather_icons[0]
-    })
+// The temperature is stored in kelvin, so we need to convert it to celsius
+function convertCalvinToCelsius(temp) {
+    return `${Math.round(temp - 273.15)}℃`
 }
