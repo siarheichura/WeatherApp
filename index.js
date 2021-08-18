26c86b8d88503176d00a28a76fc39ed14b97670f
Siarhei Chura <78800419+siarheichura@users.noreply.github.com>
2021-08-18 16:27:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// DOM-elements
 const citySelect = document.querySelector('.card__city-select')
 const city = document.querySelector('.card__city')
 const temperature = document.querySelector('.current__temperature')
@@ -8,16 +8,26 @@ const feelsLike = document.querySelector('.current__feelslike')
 const windSpeed = document.querySelector('.current__wind-speed')
 const humidity = document.querySelector('.current__humidity')
 const pressure = document.querySelector('.current__pressure')
+const forecastList = document.querySelector('.forecast-wrap')
 
-let cities = ['Minsk', 'Brest', 'Grodno', 'Vitebsk', 'Mogilev', 'Gomel']
+// required data
+let cities = ['Minsk', 'Brest', 'Hrodna', 'Vitebsk', 'Mogilev', 'Gomel']    // cities where user can find out the weather
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']   // weekdays needed for forecast
+let todayIndex = new Date().getDay();
 
 document.addEventListener("DOMContentLoaded", app)
-
 function app() {
     new Clock().start()
     setCitiesInDrop()
     showWeatherInUserGeo()
     initDropdownListener()
+}
+
+function setCitiesInDrop() {
+    cities.forEach(city => {
+        let option = new Option(city, city)
+        citySelect.append(option)
+    })
 }
 
 function initDropdownListener() {
@@ -30,18 +40,48 @@ function showWeatherInUserGeo() {
         navigator.geolocation.getCurrentPosition(position => {
         let latitude = position.coords.latitude.toFixed(4)
         let longitude = position.coords.longitude.toFixed(4)
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b5a03b378e49085452cb14ec5350c1e9`)
-        .then(response => response.json())
-        .then(result => {
-            renderWeather(result)
-        })
+            // print current weather
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b5a03b378e49085452cb14ec5350c1e9`)
+                .then(response => response.json())
+                .then(result => renderWeather(result))
+            // print forecast
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=b5a03b378e49085452cb14ec5350c1e9`)
+                .then(response => response.json())
+                .then(result => renderForecast(result))
     })
 }
 
 async function showWeatherInChosenCity(cityName) {
+    // print current weather
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b5a03b378e49085452cb14ec5350c1e9`)
     let weatherDB = await response.json()
     renderWeather(weatherDB)
+    // print forecast
+    const lon = weatherDB.coord.lon
+    const lat = weatherDB.coord.lat
+    let forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=b5a03b378e49085452cb14ec5350c1e9`)
+    let forecastDB = await forecastResponse.json()
+    renderForecast(forecastDB)
+}
+
+function getForecastTemplate(day, src, dayTemp, nightTemp) {
+    return `<div class="forecast">
+                <p class="forecast__day">${day}</p>
+                <img src="${src}" alt="" class="forecast__icon">
+                <p class="forecast__temperature-day">${dayTemp}</p>
+                <p class="forecast__temperature-night">${nightTemp}</p>
+            </div>`
+}
+
+function renderForecast(DB) {
+    forecastList.innerHTML = ''
+    for(let i = 1; i <= 3; i++) {
+        let day = days[todayIndex + i]
+        let icon =  `https://openweathermap.org/img/wn/${DB.daily[i].weather[0].icon}@2x.png`
+        let tempDay = convertCalvinToCelsius(DB.daily[i].temp.day)
+        let tempNight = convertCalvinToCelsius(DB.daily[i].temp.night)
+        forecastList.innerHTML += getForecastTemplate(day, icon, tempDay, tempNight)
+    }
 }
 
 function renderWeather(DB) {
@@ -53,13 +93,6 @@ function renderWeather(DB) {
     windSpeed.innerText = `${DB.wind.speed} m/s`
     humidity.innerText = `${DB.main.humidity} %`
     pressure.innerText = `${DB.main.pressure} hPa`
-}
-
-function setCitiesInDrop() {
-    cities.forEach(city => {
-        let option = new Option(city, city)
-        citySelect.append(option)
-    })
 }
 
 function convertCalvinToCelsius(temp) {
